172394658be096785abb36ada1f23c8eed4c2639
Sweet-Pillow <daniel_h.m@outlook.com>
2024-08-18 19:15:08 -0300

    feat: Add feat to add a letter inside the box

--- src/components/LetterBox.vue
+++ src/components/LetterBox.vue
@@ -1,11 +1,29 @@
 <script setup>
+import { ref } from 'vue'
 
-const props = defineProps(['letra'])
+const props = defineProps(['letra', 'index'])
+
+const clicked = ref(false)
+const letter = ref("")
+
+const selectLetter = () => {
+    clicked.value = true
+}
+
+const handleBlur = () => {
+    clicked.value = false
+}
+
+const getLetter = (event) => {
+    const regex = /^[a-zA-Z\s]$/;
+    regex.test(event.key)? letter.value = event.key.toUpperCase(): ""
+    console.log(event.key)
+}
 
 </script>
 
 <template>
-    <div class="font-bold size-10 border border-black flex justify-center items-center text-2xl select-none">
-        {{ props.letra }}
+    <div @click="selectLetter" @keyup="getLetter" @blur="handleBlur" tabindex="0" class="font-bold size-10 border border-black flex justify-center items-center text-2xl select-none ":class="{' border-b-4': clicked}">
+        {{ letter }}
     </div>
 </template>
--- src/components/Painel.vue
+++ src/components/Painel.vue
@@ -6,8 +6,7 @@
 <template>
     <div class="h-4/5 border border-red-700 p-4">
         <div class="flex flex-row gap-4 pb-4" v-for="_ in 5">
-
-            <LetterBox v-for="letra in palavra" :letra="letra"></LetterBox>
+            <LetterBox v-for="(letra, index) in palavra" :index="index" :letra="letra"></LetterBox>
         </div>
     </div>
 </template>
