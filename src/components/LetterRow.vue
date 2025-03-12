b1c4ccf4adaf98a26e4de10818486da8bb6c162b
Sweet-Pillow <daniel_h.m@outlook.com>
2025-03-12 15:12:55 -0300

    refactor: using a new component to render the row

---
+++ src/components/LetterRow.vue
@@ -0,0 +1,11 @@
+<script setup>
+import LetterBox from "./LetterBox.vue"
+
+const props = defineProps(['lineIndex', 'palavraCerta'])
+</script>
+
+<template>
+    <div class="flex flex-row gap-4 pb-4">
+        <LetterBox v-for="(letra, index) in 5" :index="index" :letra="letra"></LetterBox>
+    </div>
+</template>
--- src/components/Painel.vue
+++ src/components/Painel.vue
@@ -1,12 +1,10 @@
 <script setup>
-    import LetterBox from "./LetterBox.vue"
-    const palavra = "TERMO"
+import LetterRow from "./LetterRow.vue"
+const palavra = "TERMO"
 </script>
 
 <template>
     <div class="h-4/5 border border-red-700 p-4">
-        <div class="flex flex-row gap-4 pb-4" v-for="_ in 5">
-            <LetterBox v-for="(letra, index) in palavra" :index="index" :letra="letra"></LetterBox>
-        </div>
+        <LetterRow v-for="(_, index) in 6" :lineIndex="index" :palavraCerta="palavra"></LetterRow>
     </div>
 </template>
